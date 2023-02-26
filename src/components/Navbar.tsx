898509fbbc134a1a140f6820cb2a8141e680d572
y-arimura1222 <y.arimura.carecon@gmail.com>
2023-02-26 18:46:04 +0900

    Link エラー対応

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           aria-label="breadcrumb"
         >
           <Link href="/" className="text-gray-500 hover:text-gray-600">
-            {siteConfig.title}
+            {`${siteConfig.title}`}
           </Link>
           {/* Breadcrumb */}
           <Breadcrumb />
